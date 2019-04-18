d389c53e9a0510118cb26249b4328074aa1676a3
Jorge <j.l-h@hotmail.com>
2019-04-18 00:27:46 -0500

    Listas e implementadas las pantallas login y registro, menú funcional para mover las demás pantallas

--- GSmell.py
+++ GSmell.py
@@ -1,4 +1,5 @@
 import kivy
+import pymongo
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
@@ -10,11 +11,36 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
+from pymongo import MongoClient
 
 Window.clearcolor= (0.5, 0.5, 0.5, 1)
+client=MongoClient('mongodb+srv://GSmell:gsmellalce1@cluster0-sgq75.mongodb.net/test?retryWrites=true')
+db=client.GSmell
+evaluadosDB=db.Evaluados
+usuariosDB=db.Usuarios
 
 class LoginScreen(Screen):
-    pass
+    def validar_login(self):
+        usr=self.ids.inputCorreoL.text
+        contr=self.ids.inputContrasenaL.text
+        registrados=usuariosDB.find({"correo":usr , "contraseña":contr})
+        if(len(list(registrados))>0):
+            self.manager.current='PantallaScreen'
+            self.ids.loginIncorrectoLabel.text=""
+            self.ids.inputCorreoL.text=""
+            self.ids.inputContrasenaL.text=""
+        else:
+            self.ids.loginIncorrectoLabel.text="Correo o contraseña incorrecto"
+            self.ids.inputCorreoL.text=""
+            self.ids.inputContrasenaL.text=""
+
+class RegisterScreen(Screen):
+    def registrar(self):
+        new_usr={
+            "correo": self.ids.inputCorreo.text,
+            "contraseña": self.ids.inputContrasena.text
+        }
+        usuariosDB.insert_one(new_usr)
 
 class PantallaScreen(Screen):
     def mostrar_menu(self):
@@ -31,7 +57,6 @@ class Personas(Screen):
     pass
 
 class Aromas(Screen):
-
     pass
 
 class Sm(ScreenManager):
@@ -42,14 +67,25 @@ class Ssm(ScreenManager):
     
 class MenuPop(Popup):
     def pasar_a_experimentos(self):
-        pass
-    
+        megaroot=App.get_running_app()
+        megaroot.root.children[0].children[0].children[0].current='Experimentos'
+
+    def pasar_a_personas(self):
+        megaroot=App.get_running_app()
+        megaroot.root.children[0].children[0].children[0].current='Personas'
+
+    def pasar_a_aromas(self):
+        megaroot=App.get_running_app()
+        megaroot.root.children[0].children[0].children[0].current='Aromas'
+
     def ocultar_menu(self):
         self.dismiss()
 
 class GSmellApp(App):
     
     def build(self):
-        return Ssm()
+        global rooter
+        rooter=Ssm()
+        return rooter
 
 GSmellApp().run()
